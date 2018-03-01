7b575f84a80dc49b9b89431b4e05ee4261f514c4
Madeline Conner <maddyjconner@gmail.com>
2018-02-28 17:55:45 -0800

    Graph now displays in the order you would expect

--- CMPS_183/public/scripts/gpa-graph.js
+++ CMPS_183/public/scripts/gpa-graph.js
@@ -2,37 +2,56 @@
 // i.e. Fall: 3.0, Winter: 4.2, etc.
 function loadGraphData(){
     const ref = firebase.database().ref();
-    var quarterGPAs = [];
+    var usersRef = ref.child("" + userID + "/gpa");
+    
     var quarterIndex = [];
     var i = 0;
-    var numQuarters = 0;
-    var fall, spring, winter, summer;
-    var max = 4.0;
-    quarterGPAs.push(i++);
-    quarterIndex.push(0);
     var text = [];
-    var quarters = ["Winter", "Spring", "Summer", "Fall"];
-    var usersRef = ref.child("" + userID + "/gpa");
-    var dict = {};
+    var qDict = {};
+    var qfinal = [];
+    
+    // insert a 0,0 node to help scale the graph
+    qfinal.push(i++);
+    quarterIndex.push(0);
+    text.push("Q0");
+    
     
     usersRef.once("value", function(year) {
-        console.log("initial data loaded!", year.numChildren());
         year.forEach(function(quarter) {
-            console.log(quarter.val());
             quarter.forEach(function(gpa) {
-                console.log(gpa.key + " " + quarter.key);
-                quarterGPAs.push(gpa.val());
-                text.push(quarter.key + " " + gpa.key);
-                quarterIndex.push(i++);
+                qDict["" + quarter.key + gpa.key] = gpa.val();
             });
-            // now we have all "4" quarters loaded if they took all 4 quarters in that year
-        });
-        makeGraph(quarterIndex, quarterGPAs, text);
+            // copy over data based on entries in the dictionary
+            for (j = 0; j < quarter.numChildren(); j++){
+                if (a = qDict[quarter.key + "winter"]){
+                    qfinal.push(a);
+                    text.push("Winter" + quarter.key);
+                    delete(qDict[quarter.key + "winter"]);
+
+                } else if (a = qDict[quarter.key + "spring"]) {
+                    qfinal.push(a);
+                    text.push("Spring" + quarter.key);
+                    delete(qDict[quarter.key + "spring"]);
+
+                } else if (a = qDict[quarter.key + "summer"]) {
+                    qfinal.push(a);
+                    text.push("Summer" + quarter.key);
+                    delete(qDict[quarter.key + "summer"]);
+
+                } else if (a = qDict[quarter.key + "fall"]) {
+                    qfinal.push(a);
+                    text.push("Fall" + quarter.key);
+                    delete(qDict[quarter.key + "fall"]);
+                }
+                quarterIndex.push(i++);
+            }
+
+            });
+        makeGraph(quarterIndex, qfinal, text);
     });
 }
 
 function makeGraph(x, quarterGPAs, markerInfo) {
-    //console.log(x);
     var trace1 = {
       x: x, 
       y: quarterGPAs, 
